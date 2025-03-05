e882d0cf5ce123950400655d7023f599e6be9488
aerogelangels <owenjefcoat@gmail.com>
2025-03-05 01:58:21 -0600

    dropdownbar

--- funcs.js
+++ funcs.js
@@ -1,3 +1,16 @@
+var on = false;
 function dropBar() {
-    
+    let arrow = document.getElementById('arrow');
+    let dropdown = document.getElementById('dropdown');
+    let pulltab = document.getElementById('pulltab');
+    if (on == false) {
+        arrow.src="css/images/arrowUp.png";
+        dropdown.style.top = '0px';
+        pulltab.style.opacity = '0.3';
+    } else {
+        arrow.src="css/images/arrowDown.png";
+        dropdown.style.top = '-40px';
+        pulltab.style.opacity = '';
+    }
+    on = !on
 }
