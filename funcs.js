7f04424727740262a3f11abdce296456da2102a4
aerogelangels <owenjefcoat@gmail.com>
2025-07-24 03:28:10 -0500

    squors

--- funcs.js
+++ funcs.js
@@ -32,10 +32,11 @@ function dropBar() {
 }
 
 const albums = document.getElementsByClassName('album');
+var current = 1;
 function scrollAlbumsUp() {
 
 }
 
 function scrollAlbumsDown() {
-    
+
 }
