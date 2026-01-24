71d2eaac205f8ca69bed8808445627039628aa7c
aerogelangels <owenjefcoat@gmail.com>
2026-01-24 02:31:39 -0600

    yolk

--- splash.js
+++ splash.js
@@ -1,11 +1,19 @@
 const splash = document.getElementById('splash');
-const splashNum = Math.floor(Math.random() * 2);
+const splashNum = Math.floor(Math.random() * 1000);
 
-switch (splashNum) {
-    case 0:
-        splash.src = 'assets/images/splaaash.png';
+switch (true) {
+    case splashNum === 0:
+        splash.src = 'assets/images/youngcool.png';
         break;
-    case 1:
+    case (1 <= splashNum && splashNum <= 333):
+        splash.src = 'assets/images/splaaash.png';
+        document.getElementById('fungtext').classList.remove('hidden');
+        break;
+    case (334 <= splashNum && splashNum <= 666):
         splash.src = 'assets/images/humberto.png';
+        break;
+    case (667 <= splashNum && splashNum <= 999):
+        splash.src = 'assets/images/splaaash.png';
+        document.getElementById('fungtext').classList.remove('hidden');
         break;
 }
